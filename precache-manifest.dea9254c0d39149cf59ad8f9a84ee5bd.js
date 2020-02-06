f022772c94377a115b47b57a46cb99ccb5204bdd
wak <MohdTarzizi@Mohds-MacBook-Pro-2.local>
2020-02-06 19:10:42 +0800

    Updates

---
+++ precache-manifest.dea9254c0d39149cf59ad8f9a84ee5bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed85c147b2f625742c024da70475ac7",
+    "url": "/monsters-rolodex3/index.html"
+  },
+  {
+    "revision": "7cea9e1c7ef30f901b01",
+    "url": "/monsters-rolodex3/static/css/main.6082ef7d.chunk.css"
+  },
+  {
+    "revision": "e41848f17ab0028cc41f",
+    "url": "/monsters-rolodex3/static/js/2.e944fa82.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex3/static/js/2.e944fa82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cea9e1c7ef30f901b01",
+    "url": "/monsters-rolodex3/static/js/main.cb72a562.chunk.js"
+  },
+  {
+    "revision": "1caeb55b6fb44f9fcaa6",
+    "url": "/monsters-rolodex3/static/js/runtime-main.67a5d65f.js"
+  }
+]);
